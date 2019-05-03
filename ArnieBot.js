a30fc8bb6aa7bd5314370bd2838a5701b307047b
Alex <phelangood72@gmail.com>
2019-05-02 20:18:39 -0500

    Added the ability for users to $kill.

--- ArnieBot.js
+++ ArnieBot.js
@@ -7,8 +7,23 @@ let client = new Discord.Client();
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 
 client.on('message', msg => {
-  if (msg.content === 'ping')
+  if (msg.content.charAt(0) !== '$') return;
+
+  const args = msg.content.substring(1).split(' ');
+  const command = args[0]
+
+  if (command === 'ping')
     msg.reply('pong');
+  else if (command === 'kill') {
+    if (!args[1])
+      msg.reply('Oops! You didn\'t specify a target! Hopefully you weren\'t trying to kill yourself.');
+    console.log(msg.mentions.members.first());
+    msg.reply(`Congrats! You have succesfully committed first degree murder by killing ${args[1]}!`)
+  }
+  else if (command === 'help')
+    msg.reply('Not yet implemented!');
+  else
+    msg.reply('Oops! Unknown command input - please try again!')
 });
 
 client.login(auth.token);
